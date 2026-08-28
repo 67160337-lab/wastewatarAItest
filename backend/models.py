b8223e3236f3e13db8052a413ca123cf845674ea
67160337-lab <67160337@go.buu.ac.th>
2026-08-29 01:25:33 +0700

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
 from datetime import datetime
-from database import Base
+from backend.database import Base
 
 class User(Base):
     __tablename__ = "users"
