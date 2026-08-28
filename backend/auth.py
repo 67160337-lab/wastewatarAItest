1a8c2f0f6eda9779b7b1f02821de03938976b279
67160337-lab <67160337@go.buu.ac.th>
2026-08-29 02:29:18 +0700

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -3,7 +3,7 @@ from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from .models import User
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def hash_password(password: str):
     return pwd_context.hash(password[:72])
